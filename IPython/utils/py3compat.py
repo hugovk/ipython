b2061fc6954978df6eb3f561983ab1a33608f7c3
Hugo <hugovk@users.noreply.github.com>
2017-10-21 23:46:30 +0300

    Drop support for Python 2

--- IPython/utils/py3compat.py
+++ IPython/utils/py3compat.py
@@ -27,12 +27,6 @@ def cast_unicode(s, encoding=None):
 def cast_bytes(s, encoding=None):
     return encode(s, encoding)
 
-def buffer_to_bytes(buf):
-    """Cast a buffer object to bytes"""
-    if not isinstance(buf, bytes):
-        buf = bytes(buf)
-    return buf
-
 def _modify_str_or_docstring(str_change_func):
     @functools.wraps(str_change_func)
     def wrapper(func_or_str):
@@ -58,85 +52,16 @@ def safe_unicode(e):
     safe to call unicode() on.
     """
     try:
-        return unicode_type(e)
+        return str(e)
     except UnicodeError:
         pass
 
     try:
-        return str_to_unicode(str(e))
-    except UnicodeError:
-        pass
-
-    try:
-        return str_to_unicode(repr(e))
+        return repr(e)
     except UnicodeError:
         pass
 
     return u'Unrecoverably corrupt evalue'
-
-# shutil.which from Python 3.4
-def _shutil_which(cmd, mode=os.F_OK | os.X_OK, path=None):
-    """Given a command, mode, and a PATH string, return the path which
-    conforms to the given mode on the PATH, or None if there is no such
-    file.
-
-    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
-    of os.environ.get("PATH"), or can be overridden with a custom search
-    path.
-
-    This is a backport of shutil.which from Python 3.4
-    """
-    # Check that a given file can be accessed with the correct mode.
-    # Additionally check that `file` is not a directory, as on Windows
-    # directories pass the os.access check.
-    def _access_check(fn, mode):
-        return (os.path.exists(fn) and os.access(fn, mode)
-                and not os.path.isdir(fn))
-
-    # If we're given a path with a directory part, look it up directly rather
-    # than referring to PATH directories. This includes checking relative to the
-    # current directory, e.g. ./script
-    if os.path.dirname(cmd):
-        if _access_check(cmd, mode):
-            return cmd
-        return None
-
-    if path is None:
-        path = os.environ.get("PATH", os.defpath)
-    if not path:
-        return None
-    path = path.split(os.pathsep)
-
-    if sys.platform == "win32":
-        # The current directory takes precedence on Windows.
-        if not os.curdir in path:
-            path.insert(0, os.curdir)
-
-        # PATHEXT is necessary to check on Windows.
-        pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
-        # See if the given file matches any of the expected path extensions.
-        # This will allow us to short circuit when given "python.exe".
-        # If it does match, only test that one, otherwise we have to try
-        # others.
-        if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
-            files = [cmd]
-        else:
-            files = [cmd + ext for ext in pathext]
-    else:
-        # On other platforms you don't have things like PATHEXT to tell you
-        # what file suffixes are executable, so just pass on cmd as-is.
-        files = [cmd]
-
-    seen = set()
-    for dir in path:
-        normdir = os.path.normcase(dir)
-        if not normdir in seen:
-            seen.add(normdir)
-            for thefile in files:
-                name = os.path.join(dir, thefile)
-                if _access_check(name, mode):
-                    return name
-    return None
 
 # keep reference to builtin_mod because the kernel overrides that value
 # to forward requests to a frontend.
@@ -146,35 +71,19 @@ def input(prompt=''):
 builtin_mod_name = "builtins"
 import builtins as builtin_mod
 
-str_to_unicode = no_code
-unicode_to_str = no_code
 str_to_bytes = encode
 bytes_to_str = decode
-cast_bytes_py2 = no_code
 cast_unicode_py2 = no_code
-buffer_to_bytes_py2 = no_code
 
 string_types = (str,)
-unicode_type = str
 
 which = shutil.which
 
-def isidentifier(s, dotted=False):
-    if dotted:
-        return all(isidentifier(a) for a in s.split("."))
-    return s.isidentifier()
 
-xrange = range
 def iteritems(d): return iter(d.items())
 def itervalues(d): return iter(d.values())
 getcwd = os.getcwd
 
-
-def execfile(fname, glob, loc=None, compiler=None):
-    loc = loc if (loc is not None) else glob
-    with open(fname, 'rb') as f:
-        compiler = compiler or compile
-        exec(compiler(f.read(), fname, 'exec'), glob, loc)
 
 # Refactor print statements in doctests.
 _print_statement_re = re.compile(r"\bprint (?P<expr>.*)$", re.MULTILINE)
